ae3d545f3df3995ef8bf74563cbbf987e647fce0
Fernando Matheus <fernandomatheuscl@gmail.com>
2021-01-27 22:26:57 -0300

    chore: sharing deps to avoid downloading them  multiple times

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -2,6 +2,8 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
+const packageJson = require('../package.json');
+
 const commonConfig = require('./webpack.common');
 
 const devConfig = {
@@ -17,7 +19,8 @@ const devConfig = {
       name: 'container',
       remotes: {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-      }
+      },
+      shared: packageJson.dependencies
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -2,6 +2,8 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
+const packageJson = require('../package.json');
+
 const commonConfig = require('./webpack.common');
 
 const devConfig = {
@@ -19,6 +21,7 @@ const devConfig = {
       exposes: {
         './MarketingApp': './src/bootstrap'
       },
+      shared: packageJson.dependencies
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
