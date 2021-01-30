a09eba4a7fb384cd37d11020a9014d858eb352a2
Fernando Matheus <fernandomatheuscl@gmail.com>
2021-01-30 17:20:37 -0300

    chore: test container pipeline

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return <div>
-    <h1>This is the container app!!!</h1>
+    <h1>Testing container pipeline</h1>
     <hr />
     <MarketingApp />
   </div>
