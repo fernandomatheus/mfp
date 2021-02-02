772cdfcddabd5824866e8c8ed6471e9bc098ef88
Fernando Matheus <fernandomatheuscl@gmail.com>
2021-02-02 20:10:08 -0300

    chore: added invalidation step to  build

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return <div>
-    <h1>Testing container pipeline!!</h1>
+    <h1>Testing container pipeline!!!</h1>
     <hr />
     <MarketingApp />
   </div>
